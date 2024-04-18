01d92358dad7dfca6aa3dc9c86ccdaf262018cb2
Ravi Singh Bajetha <singhravi.rs11@gmail.com>
2024-04-18 18:22:26 +0530

    NavBar

--- client/src/store/auth.js
+++ client/src/store/auth.js
@@ -1,15 +1,29 @@
 import {createSlice}from '@reduxjs/toolkit'
 const initialState={
-    isAuth:false
+    isAuth:false,
+    isLoginPage:false,
+    isSignUpPage:true
 }
 const authSlice=createSlice({
     name:'auth',
     initialState:initialState,
     reducers:{
  setIsAuth(state){
-    state.isAuth=!state.isAuth
+    state.isAuth=true
+    state.isLoginPage=false
+state.isSignUpPage=false
+ },
+ setIsLogin(state){
+state.isLoginPage=false
+state.isSignUpPage=true
+state.isAuth=false
+ },
+ setSignUp(state){
+state.isSignUpPage=false
+state.isLoginPage=true
+state.isAuth=false
  }
     }
 })
-export const {setIsAuth}=authSlice.actions
+export const {setIsAuth,setIsLogin,setSignUp}=authSlice.actions
 export const authStoreReducer=authSlice.reducer
