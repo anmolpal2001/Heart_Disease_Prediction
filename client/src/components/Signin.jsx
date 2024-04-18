e664081ee91bb7807753b623142f6613b63562db
Ravi Singh Bajetha <singhravi.rs11@gmail.com>
2024-04-19 01:13:39 +0530

    Sign in page  updated

--- client/src/components/Signin.jsx
+++ client/src/components/Signin.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Signin = () => {
   return (
-    <div>
-      <div class="flex flex-col w-full max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
-        <div class="self-center mb-6 text-xl font-light text-gray-600 sm:text-2xl dark:text-white">
+    <div className="flex justify-center items-center mt-20">
+      <div className="flex flex-col w-full max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
+        <div className="self-center mb-6 text-xl font-light text-gray-600 sm:text-2xl dark:text-white">
           Login To Your Account
         </div>
-        <div class="mt-8">
-          <form action="#" autoComplete="off">
-            <div class="flex flex-col mb-2">
-              <div class="flex relative ">
-                <span class="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+        <div className="mt-8">
+          <form>
+            <div className="flex flex-col mb-2">
+              <div className="flex relative ">
+                <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
                   <svg
                     width="15"
                     height="15"
@@ -24,15 +24,15 @@ const Signin = () => {
                 </span>
                 <input
                   type="text"
-                  id="sign-in-email"
-                  class=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                  id="email"
+                  className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                   placeholder="Your email"
                 />
               </div>
             </div>
-            <div class="flex flex-col mb-6">
-              <div class="flex relative ">
-                <span class="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+            <div className="flex flex-col mb-6">
+              <div className="flex relative ">
+                <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
                   <svg
                     width="15"
                     height="15"
@@ -45,40 +45,40 @@ const Signin = () => {
                 </span>
                 <input
                   type="password"
-                  id="sign-in-email"
-                  class=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                  id="password"
+                  className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                   placeholder="Your password"
                 />
               </div>
             </div>
-            <div class="flex items-center mb-6 -mt-4">
-              <div class="flex ml-auto">
+            <div className="flex items-center mb-6 -mt-4">
+              <div className="flex ml-auto">
                 <a
                   href="#"
-                  class="inline-flex text-xs font-thin text-gray-500 sm:text-sm dark:text-gray-100 hover:text-gray-700 dark:hover:text-white"
+                  className="inline-flex text-xs font-thin text-gray-500 sm:text-sm dark:text-gray-100 hover:text-gray-700 dark:hover:text-white"
                 >
                   Forgot Your Password?
                 </a>
               </div>
             </div>
-            <div class="flex w-full">
+            <div className="flex w-full">
               <button
                 type="submit"
-                class="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                className="py-2 px-4  bg-[#2A8683]  text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
               >
                 Login
               </button>
             </div>
           </form>
         </div>
-        <div class="flex items-center justify-center mt-6">
-          <a
-            href="#"
-            target="_blank"
-            class="inline-flex items-center text-xs font-thin text-center text-gray-500 hover:text-gray-700 dark:text-gray-100 dark:hover:text-white"
+        <div className="flex items-center justify-center mt-6">
+          <Link
+            to='/sign-up'
+       
+            className="inline-flex items-center text-xs font-thin text-center text-gray-500 hover:text-gray-700 dark:text-gray-100 dark:hover:text-white"
           >
-            <span class="ml-2">You don&#x27;t have an account?</span>
-          </a>
+            <span className="ml-2">You don&#x27;t have an account?</span>
+          </Link>
         </div>
       </div>
     </div>
