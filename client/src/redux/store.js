a1d13f4a563ccdec66a96cd2e6f58374ac2f2909
anmolpal2001 <anmolpal2001@gmail.com>
2024-04-18 23:07:55 +0530

    redux issue solved

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore,combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
-import studentReducer from "./student/studentSlice";
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' 
 
 const rootReducer = combineReducers({
   auth : authReducer,
-  student : studentReducer,
 });
 
 const persistConfig = {
