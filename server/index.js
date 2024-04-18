1fc5bf3298e4efe591f42e0731aed5b4d5574524
anmolpal2001 <anmolpal2001@gmail.com>
2024-04-19 00:21:42 +0530

    user route added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import userRoutes from './routes/user.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,8 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+app.use('/api/v1/auth', userRoutes);
 
 
 app.listen(PORT, () => {
