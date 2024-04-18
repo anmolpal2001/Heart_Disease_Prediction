f7018e9542e84cc3fe8cf5b82eb505784751798c
Ravi Singh Bajetha <singhravi.rs11@gmail.com>
2024-04-19 01:23:24 +0530

    useState added in Signin

--- client/src/components/Signin.jsx
+++ client/src/components/Signin.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
 const Signin = () => {
+  const [data,setData]=useState({
+    email:'',
+    password:''
+  })
+  const submitHandler=(event)=>{
+  event.preventDefault()
+  console.log(data.email +" "+data.password)
+  setData(()=> {return {
+    email:'',
+    password:''
+  }})
+  }
+  const onChangeHandler=(event)=>{
+  setData((prev)=>{
+    return {
+      ...prev,
+      [event.target.id]:event.target.value
+    }
+  })
+  
+  }
   return (
     <div className="flex justify-center items-center mt-20">
       <div className="flex flex-col w-full max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
@@ -8,7 +30,7 @@ const Signin = () => {
           Login To Your Account
         </div>
         <div className="mt-8">
-          <form>
+          <form onSubmit={submitHandler}>
             <div className="flex flex-col mb-2">
               <div className="flex relative ">
                 <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
@@ -23,7 +45,9 @@ const Signin = () => {
                   </svg>
                 </span>
                 <input
+                onChange={onChangeHandler}
                   type="text"
+                  value={data.email}
                   id="email"
                   className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                   placeholder="Your email"
@@ -48,6 +72,8 @@ const Signin = () => {
                   id="password"
                   className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                   placeholder="Your password"
+                  value={data.password}
+                  onChange={onChangeHandler}
                 />
               </div>
             </div>
