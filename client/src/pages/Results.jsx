a5b723fdfbb12ba2590576515657e40d366ed023
anmolpal2001 <anmolpal2001@gmail.com>
2024-04-21 06:05:01 +0530

    results update

--- client/src/pages/Results.jsx
+++ client/src/pages/Results.jsx
@@ -54,24 +54,21 @@ function Results() {
       <div className="bg-[#F7F7F7] border-gray-200 px-4 lg:px-6 py-2.5">
         <div className="mx-auto max-w-screen-2xl flex flex-col justify-center items-center">
           <div className="px-4 lg:px-14 max-w-screen-2xl w-full mx-auto h-screen">
-            <div className="my-10 flex flex-col items-center justify-between gap-12">
-              {DUMMy.map((data, index) => (
-                <div
-                  className="w-1/2 flex justify-center items-center bg-green-300"
-                  key={index}
-                >
-                  <Result
-                    toggleResult={toggle}
-                    index={index}
-                    show={index === openIndex}
-                    date={data.data}
-                    reportId={data.reportId}
-                  />
-                </div>
-              ))}
+              <div className="my-10 flex flex-col items-center justify-between gap-12">
+                {DUMMy.map((data, index) => (
+                  <div className="w-1/2 flex justify-center items-center bg-green-300" key={index}>
+                    <Result
+                      toggleResult={toggle}
+                      index={index}
+                      show={index === openIndex}
+                      date={data.data}
+                      reportId={data.reportId}
+                    />
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
       </div>
     </>
   );
