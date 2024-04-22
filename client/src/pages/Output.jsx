9ceeaa235ecbcfd649d28c39f708ba17c23d2ea0
Saurav <sauravdhyani12@gmail.com>
2024-04-22 20:09:16 +0530

    output page update

--- client/src/pages/Output.jsx
+++ client/src/pages/Output.jsx
@@ -2,70 +2,77 @@ import React, { useState } from "react";
 import Form from "./Form";
 import { MdHealthAndSafety } from "react-icons/md";
 import { FaThumbsUp } from "react-icons/fa";
+import { FaThumbsDown } from "react-icons/fa";
+
+import { FaDownLong } from "react-icons/fa6";
+import { FaRegSmile } from "react-icons/fa";
+
+import { FaSmile } from "react-icons/fa";
 
 import { MdDownload } from "react-icons/md";
 
 import { Link } from "react-router-dom";
+
 //try
 
 function Output() {
-  const [yes, setYes] = useState(true);
+  const [yes, setYes] = useState(false);
   const diagnosis = yes
-    ? "Our analysis indicates no evidence of heart disease based on the provided data."
-    : "Our analysis indicates the presence of heart disease based on the provided data.";
+    ? "Our Analysis Indicates No Evidence Of Heart Disease Based On The Provided Data."
+    : "Our Analysis Indicates The Presence Of Heart Disease Based On The Provided Data.";
 
   const Recommendations = yes ? (
     <div>
-      <p>
-        <span className="font-bold">Consult a Healthcare Professional :</span>{" "}
+      <p className="my-2">
+        <span className="font-bold ">Consult a Healthcare Professional :</span>{" "}
         We recommend seeking advice from a qualified medical practitioner for
         further evaluation and personalized treatment.
       </p>
-      <p>
-        <span className="font-bold">Lifestyle Modifications</span>
-        Consider adopting a heart-healthy lifestyle, including regular exercise,
-        a balanced diet, and stress management, to manage heart disease risk
+      <p className="my-2">
+        <span className="font-bold">Lifestyle Modifications :</span> Consider
+        adopting a heart-healthy lifestyle, including regular exercise, a
+        balanced diet, and stress management, to manage heart disease risk
         factors.
       </p>
     </div>
   ) : (
     <div>
-      <p>
-        <span className="font-bold">Maintain Healthy Habits</span>: Continue
+      <p className="my-2">
+        <span className="font-bold ">Maintain Healthy Habits :</span> Continue
         prioritizing a healthy lifestyle, including regular exercise, a balanced
         diet, and stress management, to support overall heart health.
       </p>
-      <p>
-        <span className="font-bold">Regular Health Check-ups:</span>
-        Schedule periodic check-ups with your healthcare provider to monitor
-        your cardiovascular health and address any emerging concerns.
+      <p className="my-2">
+        <span className="font-bold">Regular Health Check-ups :</span> Schedule
+        periodic check-ups with your healthcare provider to monitor your
+        cardiovascular health and address any emerging concerns.
       </p>
     </div>
   );
   const nextStep = !yes ? (
     <div>
-      <p>
+      <p className="my-2">
         <span className="font-bold">Preventive Measures :</span> Stay proactive
         in managing heart disease risk factors by maintaining a healthy weight,
         avoiding smoking, and managing stress.
       </p>
-      <p>
-        <span className="font-bold">Monitor Symptoms</span>
-        Pay attention to any changes in your health and consult your healthcare
-        provider if you experience symptoms of concern.
+      <p className="my-2">
+        <span className="font-bold">Monitor Symptoms :</span> Pay attention to
+        any changes in your health and consult your healthcare provider if you
+        experience symptoms of concern.
       </p>
     </div>
   ) : (
     <div>
-      <p className="text-[#837d7d]">
-        <span className="font-bold">Medical Consultation</span>: Schedule an
+      <p className="my-2">
+        <span className="font-bold">Medical Consultation : </span> Schedule an
         appointment with a cardiologist or healthcare provider for a
         comprehensive evaluation and treatment plan.
       </p>
-      <p>
-        <span className="font-bold">Follow-up Monitoring</span>: Stay proactive
-        in your health by regularly monitoring your condition and adhering to
-        the prescribed treatment regimen.
+      <p className="my-2">
+        <span className="font-bold">Follow-up Monitoring : </span> Stay
+        proactive in your health by regularly monitoring your condition and
+        adhering to the prescribed treatment regimen.
       </p>
     </div>
   );
@@ -76,51 +83,71 @@ function Output() {
         {/* <MdHealthAndSafety className="text-[#2A8683] text-6xl mb-4" /> */}
 
         <h1 className="text-4xl font-bold mb-4 text-center text-[#2A8683]">
-          Your Health Test Report
+          Your Heart Report
         </h1>
 
-        {/* Render report details from the output state */}
-        <div className="mb-4">
-          {/* Assuming output contains the report data */}
-          <p>Name : naam</p>
-          <p>Age : 4</p>
-          <p>Gender : mada</p>
-          {/* Render other report details similarly */}
+        <div className="mb-4 w-[95%] sm:w-1/2">
+          <div className="flex items-center justify-start ">
+            <span className="w-1/2">Name</span>
+            <span className="w-1/2">: name</span>
+          </div>
+          <div className="flex items-center justify-start ">
+            <span className="w-1/2">Age</span>
+            <span className="w-1/2">: name</span>
+          </div>
+          <div className="flex items-center justify-start ">
+            <span className="w-1/2">Gender</span>
+            <span className="w-1/2">: name</span>
+          </div>
         </div>
         <div>
           <div className="flex justify-center items-center">
-            <FaThumbsUp className="text-[#2A8683] text-6xl mb-4 " />
+            {/* {yes ? (
+              <FaThumbsUp className="text-[#2A8683] text-6xl mb-4 " />
+            ) : (
+              <FaThumbsDown className="text-[#2A8683] text-6xl mb-4 " />
+            )}
+            <FaSmile className="text-[#2A8683] text-6xl mb-4 " /> */}
+            <FaRegSmile className="text-[#2A8683] text-6xl mb-4 " />
           </div>
 
           <p className="mb-4 text-center">
-            <span className="font-semibold">Result:</span> message
+            <span className="font-semibold text-3xl text-[#2A8683]">
+              Result{" "}
+            </span>
           </p>
         </div>
 
         <div className="flex m-2 items-center justify-center border-t border-gray-400 w-full h-0"></div>
 
         <div className="m-4">
-          <p className="text-[#2A8683] text-2xl text-center ">Diagnosis</p>
-          <p className="text-center">{diagnosis}</p>
+          {/* <p className="text-[#2A8683] text-2xl text-center font-bold">
+            Diagnosis
+          </p> */}
+          <p className="text-center text-lg">{diagnosis}</p>
         </div>
         <div className="flex m-2 items-center justify-center border-t border-gray-400 w-full h-0"></div>
 
         <div className="m-2">
-          <p className="text-[#2A8683] text-2xl text-center">Recommendations</p>
+          <p className="text-[#2A8683] text-2xl text-center font-bold mb-1">
+            Recommendations
+          </p>
           <div>{Recommendations}</div>
         </div>
         <div className="flex m-2 items-center justify-center border-t border-gray-400 w-full h-0"></div>
 
         <div className="m-2">
-          <p className="text-[#2A8683] text-2xl text-center">Next Steps</p>
+          <p className="text-[#2A8683] text-2xl text-center mb-1 font-bold">
+            Next Steps
+          </p>
           <div>{nextStep}</div>
         </div>
 
         <button className="m-2 hover:translate-y-1 transition-transform duration-300">
           <Link to="/reports" className="flex text-lg text-[#2A8683]">
             View Full Report
-            <span className="flex justify-center mx-1 items-center text-[#2A8683] text-2xl">
-              <MdDownload />
+            <span className="flex justify-center mx-1 items-center text-[#2A8683] text-xl -rotate-90">
+              <FaDownLong />
             </span>
           </Link>
         </button>
