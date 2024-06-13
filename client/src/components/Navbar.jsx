91186fd2c556e908a80abf7b39d4a2dd516209bb
Ravi Singh Bajetha <singhravi.rs11@gmail.com>
2024-06-13 15:30:21 +0530

    temperary

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
                 to="sign-in"
                 className="text-white bg-[#2A8683]  font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
               >
-                Get Started
+              Login
               </Link>
             </div>
           )}
